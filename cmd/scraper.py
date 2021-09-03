1991bd1e930badf3a35609ce44a778ed5da71989
TonyTromp <tony.tromp@gmail.com>
2021-09-04 00:34:54 +0200

    scraper.py added

---
+++ cmd/scraper.py
@@ -0,0 +1,34 @@
+#!/bin/python
+
+import os
+import subprocess
+
+cmd='docker exec -ti docker_tidal-connect_1 /usr/bin/tmux capture-pane -pS -10'
+
+stdout = subprocess.check_output(cmd.split());
+
+WINDOW_SIZE=40
+WINDOW_COUNT=2
+
+VALUE_MAP = {}
+
+
+for line in stdout.decode('utf-8').splitlines():
+  #print(line)
+  if line.startswith('PlaybackState::'):
+     VALUE_MAP['playback_state']=line.split('::')[1]
+
+  if line.startswith('xx',WINDOW_SIZE-1):
+    for window_cnt in range(WINDOW_COUNT):
+      str_keyvals = (line[(WINDOW_SIZE*window_cnt)+1:(WINDOW_SIZE*(window_cnt+1))-1].strip())
+      #print(str_keyvals)
+
+      ar_props = str_keyvals.split(':')
+      if len(ar_props)>1:
+        key=(ar_props[0].replace(' ','_'));
+        value=''.join(ar_props[1:]).strip()
+        sess_state_prefix='SessionState'
+        if value.startswith(sess_state_prefix):
+           value = value[len(sess_state_prefix):] 
+        VALUE_MAP[key]=value
+print(VALUE_MAP)
